68dda84699c9621f3dec71b914ae62f49e63c38d
Puneet Kala <puneet.0191@gmail.com>
2022-06-09 17:45:03 +0530

    PMM-9951 removing mocha-multi

--- pmm_pgsm_setup/pmm_pgsm_setup.sh
+++ pmm_pgsm_setup/pmm_pgsm_setup.sh
@@ -88,5 +88,5 @@ git clone -b ${PMM_UI_BRANCH} https://github.com/percona/pmm-ui-tests
 pushd pmm-ui-tests
 npm install --force
 export PMM_UI_URL="http://127.0.0.1:8081/"
-./node_modules/.bin/codeceptjs run --debug --steps --reporter mocha-multi -c pr.codecept.js --grep '@pgsm-pmm-integration'
+./node_modules/.bin/codeceptjs run --debug --steps -c pr.codecept.js --grep '@pgsm-pmm-integration'
 popd
