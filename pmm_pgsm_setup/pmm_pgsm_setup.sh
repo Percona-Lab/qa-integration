d31435b538300d61c5fce4a887614d610eccceae
Puneet Kala <puneet.0191@gmail.com>
2022-09-05 18:54:18 +0530

    PMM-7 Modify to release branch REL_1_1_0

--- pmm_pgsm_setup/pmm_pgsm_setup.sh
+++ pmm_pgsm_setup/pmm_pgsm_setup.sh
@@ -9,7 +9,7 @@ export PMM_CONTAINER_PORT=443
 export PMM_CONTAINER_IMAGE=perconalab/pmm-server:dev-latest
 export PMM_UI_BRANCH=main
 export PMM_QA_GIT_BRANCH=main
-export PGSTAT_MONITOR_BRANCH=main
+export PGSTAT_MONITOR_BRANCH=REL_1_1_0
 export PGSQL_VERSION=14
 export PGSTAT_MONITOR_REPO=percona/pg_stat_monitor
 
