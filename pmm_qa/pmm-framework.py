71ddd4db840434b25b3f6d33eb0844a0cc962348
Saikumar <saikumar.vs@percona.com>
2024-04-15 12:32:38 +0530

    Updates for HAPROXY Setup

--- pmm_qa/pmm-framework.py
+++ pmm_qa/pmm-framework.py
@@ -28,7 +28,12 @@ database_configs = {
         "versions": ["11", "12", "13", "14", "15", "16"],
         "configurations": {"CLIENT_VERSION": "3-dev-latest", "USE_SOCKET": ""}
     },
+    "HAPROXY": {
+        "versions": [""],
+        "configurations": {"CLIENT_VERSION": "3-dev-latest"}
+    },
 }
+
 
 def run_ansible_playbook(playbook_filename, env_vars, args):
     # Install Ansible
@@ -220,6 +225,29 @@ def setup_pgsql(db_type, db_version=None, db_config=None, args=None):
 
     # Ansible playbook filename
     playbook_filename = 'pgsql_pgss_setup.yml'
+
+    # Call the function to run the Ansible playbook
+    run_ansible_playbook(playbook_filename, env_vars, args)
+
+
+def setup_haproxy(db_type, db_version=None, db_config=None, args=None):
+    # Check if PMM server is running
+    container_name = get_running_container_name()
+    if container_name is None and args.pmm_server_ip is None:
+        print(f"Check if PMM Server is Up and Running..Exiting")
+        exit()
+
+    # Define environment variables for playbook
+    env_vars = {
+        'PMM_SERVER_IP': args.pmm_server_ip or container_name or '127.0.0.1',
+        'HAPROXY_CONTAINER': 'haproxy_pmm',
+        'CLIENT_VERSION': get_value('CLIENT_VERSION', db_type, args, db_config),
+        'ADMIN_PASSWORD': os.getenv('ADMIN_PASSWORD') or args.pmm_server_password or 'admin',
+        'PMM_QA_GIT_BRANCH': os.getenv('ADMIN_PASSWORD') or 'v3'
+    }
+
+    # Ansible playbook filename
+    playbook_filename = 'haproxy_setup.yml'
 
     # Call the function to run the Ansible playbook
     run_ansible_playbook(playbook_filename, env_vars, args)
@@ -423,6 +451,8 @@ def setup_database(db_type, db_version=None, db_config=None, args=None):
         setup_pdpgsql(db_type, db_version, db_config, args)
     elif db_type == 'PSMDB':
         setup_psmdb(db_type, db_version, db_config, args)
+    elif db_type == 'HAPROXY':
+        setup_haproxy(db_type, db_version, db_config, args)
     else:
         print(f"Database type {db_type} is not recognised, Exiting...")
         exit(1)
