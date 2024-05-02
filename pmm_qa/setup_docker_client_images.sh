96ac6481944e52259858eb8760267e3bd4d1575d
Saikumar <saikumar.vs@percona.com>
2024-05-02 11:55:59 +0530

    Review Updates

--- pmm_qa/setup_docker_client_images.sh
+++ pmm_qa/setup_docker_client_images.sh
@@ -9,7 +9,7 @@ docker-compose -f docker-compose-clients.yaml build --no-cache
 docker-compose -f docker-compose-clients.yaml up -d
 echo "Adding DB Clients to PMM Server"
 docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3309 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true
-docker exec pmm-client-1 pmm-admin add postgresql --query-source=pgstatements --username=pmm --password=pmm-pass --service-name=postgres-16 --host=postgres-1 --port=5432 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true
+docker exec pmm-client-1 pmm-admin add postgresql --query-source=pgstatements --username=pmm --password=pmm-pass --service-name=pdpgsql-1 --host=postgres-1 --port=5432 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true
 docker exec pmm-client-1 pmm-admin add mongodb --username=pmm --password=pmm-pass --service-name=mongodb-7.0  --host=psmdb-1 --port=27037 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true
 
 
