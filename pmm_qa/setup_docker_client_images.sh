db61be6eda5419f6d70d7ab7bfac7ed48bdb86a5
Saikumar <saikumar.vs@percona.com>
2024-11-04 11:46:57 +0530

    Fix integration test issues

--- pmm_qa/setup_docker_client_images.sh
+++ pmm_qa/setup_docker_client_images.sh
@@ -5,9 +5,9 @@ echo
 echo "Configuring Multiple Docker Images with PMM Server and Client"
 echo "Please wait...."
 docker network create docker-client-check || true
-docker-compose -f docker-compose-clients.yaml down -v --remove-orphans
-docker-compose -f docker-compose-clients.yaml build --no-cache
-docker-compose -f docker-compose-clients.yaml up -d
+docker compose -f docker-compose-clients.yaml down -v --remove-orphans
+docker compose -f docker-compose-clients.yaml build --no-cache
+docker compose -f docker-compose-clients.yaml up -d
 sleep 20
 echo "Adding DB Clients to PMM Server"
 docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3306 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true
