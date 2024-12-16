dd28c7290e92bf1a71f39e7e086d821e98fbb1a0
Peter Sirotnak <sirotnak.p@gmail.com>
2024-12-16 11:53:11 +0100

    PMM-7: Add arbiter to setup

--- pmm_psmdb-pbm_setup/start-sharded.sh
+++ pmm_psmdb-pbm_setup/start-sharded.sh
@@ -135,7 +135,7 @@ docker compose -f docker-compose-sharded.yaml exec -T rscfg01 mongo --quiet << E
         {
             "_id" : 2,
             "host" : "rscfg03:27017",
-            "priority": 1
+            "arbiterOnly": true
         }
       ]
       };
