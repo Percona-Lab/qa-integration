ad6e58f0df182570e1a786eb38d1c258f8ce036a
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-15 18:00:13 +0100

    PMM-13481: Log running containers

--- pmm_qa/pmm-framework.py
+++ pmm_qa/pmm-framework.py
@@ -81,6 +81,8 @@ def run_ansible_playbook(playbook_filename, env_vars, args):
     if args.verbose:
         print(f'Options set after considering Defaults: {env_vars}')
 
+    print(f'Verbose level is: {args.verboseLevel}')
+
     r = ansible_runner.run(
         private_data_dir=script_dir,
         playbook=playbook_path,
@@ -188,6 +190,11 @@ def setup_ps(db_type, db_version=None, db_config=None, args=None):
 
     # Ansible playbook filename
     playbook_filename = 'ps_pmm_setup.yml'
+
+    verboseLevel = os.environ.get('VERBOSE_LEVEL')
+
+    if verboseLevel is not None:
+      args.verboseLeve = verboseLevel
 
     # Call the function to run the Ansible playbook
     run_ansible_playbook(playbook_filename, env_vars, args)
