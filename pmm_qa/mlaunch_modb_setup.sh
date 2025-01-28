c6f4afd7ca8a6e95e005b3f8b84b34ac07f5257b
Saikumar <saikumar.vs@percona.com>
2025-01-28 11:48:37 +0530

    Updates to Support Community Versions

--- pmm_qa/mlaunch_modb_setup.sh
+++ pmm_qa/mlaunch_modb_setup.sh
@@ -9,7 +9,7 @@ while [ $# -gt 0 ]; do
 done
 
 if [ -z "$mongodb_version" ]; then
-  export mongodb_version=6.0
+  export mongodb_version=7.0
 fi
 
 if [ -z "$mongdb_setup" ]; then
@@ -35,41 +35,18 @@ sleep 10
 #chmod +x mongo_startup.sh
 export SERVICE_RANDOM_NUMBER=$(echo $((1 + $RANDOM % 9999)))
 
-### Detect latest tarball link for specified mongodb_version: 8.0 | 7.0 | 6.0 | 5.0 | 4.4 | 4.2 at the moment
-psmdb_latest=$( wget -q --post-data "version=percona-server-mongodb-${mongodb_version}" https://www.percona.com/products-api.php -O - | grep  -oP "(?<=value\=\")[^\"]*" | sort -V | tail -1)
-if [[ "$mongodb_version" == "4.4" ]]; then
-   psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep glibc2\.17-minimal)
-else
-   psmdb_tarball=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep focal-minimal)
-fi
+## Detect latest tarball link for specified mongodb_version: 7.0 | 6.0 | 5.0 | 4.4 | 4.2 at the moment
+modb_tarball=$(curl -s "https://www.mongodb.com/try/download/community" | \
+  grep -oP "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-${mongodb_version//./\\.}\.\d+\.tgz" | \
+  sort -V | tail -n 1)
 
-echo "Downloading ${mongodb_version} ..."
-wget -O percona_server_mongodb.tar.gz ${psmdb_tarball}
+echo "Downloading ${mongodb_version} from ${modb_tarball}..."
+wget -O percona_server_mongodb.tar.gz ${modb_tarball}
 tar -xvf percona_server_mongodb.tar.gz
 
-export extracted_folder_name=$(ls | grep percona-server-mongodb)
+export extracted_folder_name=$(ls | grep mongodb-linux)
 echo "Extracted folder name ${extracted_folder_name}"
-mv ${extracted_folder_name} psmdb_${mongodb_version}
-
-# TODO: refactor if to match range of versions 6.0+
-if [[ "$mongodb_version" == "6.0" || "$mongodb_version" == "7.0" || "$mongodb_version" == "8.0" ]]; then
-### PSMDB 6+ requires "percona-mongodb-mongosh" additionally
-    if [[ "$mongodb_version" == "8.0" ]]; then
-      # Use Mongo 7.0 mongosh itself for 8.0
-      psmdb_latest=$(wget -q --post-data "version=percona-server-mongodb-7.0" https://www.percona.com/products-api.php -O - | grep  -oP "(?<=value\=\")[^\"]*" | sort -V | tail -1)
-      mongosh_link=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep mongosh || true)
-      if [ -z "$mongosh_link" ]; then
-            psmdb_latest=$(wget -q --post-data "version=percona-server-mongodb-6.0" https://www.percona.com/products-api.php -O - | grep  -oP "(?<=value\=\")[^\"]*" | sort -V | tail -1)
-      fi
-    fi
-    mongosh_link=$(wget -q --post-data "version_files=${psmdb_latest}&software_files=binary" https://www.percona.com/products-api.php -O - | jq -r '.[] | select(.link | contains("sha") | not) | .link' | grep mongosh)
-    echo "Downloading mongosh ${mongosh_link}..."
-    wget -O mongosh.tar.gz ${mongosh_link}
-    tar -xvf mongosh.tar.gz
-    mv percona-mongodb-mongosh* mongosh
-    cp mongosh/bin/mongosh ./psmdb_${mongodb_version}/bin/mongo
-    rm mongosh.tar.gz
-fi
+mv ${extracted_folder_name} modb_${mongodb_version}
 rm percona_server_mongodb.tar.gz*
 
 # For mongodb dependency in Debian
@@ -77,7 +54,7 @@ wget http://http.us.debian.org/debian/pool/main/o/openldap/libldap-2.4-2_2.4.47+
 apt install -y ./libldap-2.4-2_2.4.47+dfsg-3+deb10u7_amd64.deb
 
 if [ "$mongodb_setup" == "pss" ]; then
-    mlaunch init --bind_ip 0.0.0.0 --binarypath "./psmdb_${mongodb_version}/bin" --replicaset --name rs1 --nodes 3
+    mlaunch init --bind_ip 0.0.0.0 --binarypath "./modb_${mongodb_version}/bin" --replicaset --name rs1 --nodes 3
     sleep 20
     pmm-admin remove mongodb mongodb_rs1_1_${SERVICE_RANDOM_NUMBER} || true; pmm-admin add mongodb --enable-all-collectors --cluster mongodb_node_cluster --replication-set=rs1 --environment=mongodb_rs_node --metrics-mode=$metrics_mode mongodb_rs1_1_${SERVICE_RANDOM_NUMBER} --debug 127.0.0.1:27017
     sleep 2
@@ -88,7 +65,7 @@ if [ "$mongodb_setup" == "pss" ]; then
 fi
 
 if [ "$mongodb_setup" == "psa" ]; then
-    mlaunch init --bind_ip 0.0.0.0 --binarypath "./psmdb_${mongodb_version}/bin" --replicaset --name rs1 --nodes 2 --arbiter
+    mlaunch init --bind_ip 0.0.0.0 --binarypath "./modb_${mongodb_version}/bin" --replicaset --name rs1 --nodes 2 --arbiter
     sleep 20
     pmm-admin remove mongodb mongodb_rs1_1_${SERVICE_RANDOM_NUMBER} || true; pmm-admin add mongodb --enable-all-collectors --cluster mongodb_node_cluster --replication-set=rs1 --environment=mongodb_rs_node --metrics-mode=$metrics_mode mongodb_rs1_1_${SERVICE_RANDOM_NUMBER} --debug 127.0.0.1:27017
     sleep 2
@@ -99,7 +76,7 @@ if [ "$mongodb_setup" == "psa" ]; then
 fi
 
 if [ "$mongodb_setup" == "sharded" ]; then
-    mlaunch init --bind_ip 0.0.0.0 --binarypath "./psmdb_${mongodb_version}/bin" --replicaset --sharded rs1 rs2 --config 3
+    mlaunch init --bind_ip 0.0.0.0 --binarypath "./modb_${mongodb_version}/bin" --replicaset --sharded rs1 rs2 --config 3
     pmm-admin add mongodb --enable-all-collectors --cluster mongodb_node_cluster --environment=mongos_shraded_node mongos_shraded_node_${SERVICE_RANDOM_NUMBER} --metrics-mode=$metrics_mode --debug 127.0.0.1:27017
     sleep 2
     pmm-admin add mongodb --enable-all-collectors --cluster mongodb_node_cluster --replication-set=config --environment=mongodb_config_node mongodb_config_1_${SERVICE_RANDOM_NUMBER} --metrics-mode=$metrics_mode --debug 127.0.0.1:27024
