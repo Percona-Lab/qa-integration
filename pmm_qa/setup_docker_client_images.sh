973356bbccbef686a53271a5ee1fb9aed8fa7bbe
Peter Sirotnak <sirotnak.p@gmail.com>
2025-03-06 11:24:44 +0100

    PMM-13626: use correct docker container for pmm client

--- pmm_qa/setup_docker_client_images.sh
+++ pmm_qa/setup_docker_client_images.sh
@@ -7,7 +7,8 @@ echo "Please wait...."
 docker network create docker-client-check || true
 docker compose -f docker-compose-clients.yaml down -v --remove-orphans
 docker compose -f docker-compose-clients.yaml build --no-cache
-docker compose -f docker-compose-clients.yaml up -d
+echo "$CLIENT_DOCKER_VERSION is the client docker version"
+CLIENT_DOCKER_VERSION=${CLIENT_DOCKER_VERSION} docker compose -f docker-compose-clients.yaml up -d
 sleep 20
 echo "Adding DB Clients to PMM Server"
 docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3306 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true
