b9361060da1035d9dfbb2bc55d58cf988c75995e
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-03 09:15:33 +0200

    PMM-7: Start docker container

--- pmm_qa/percona_server/install_percona_server.py
+++ pmm_qa/percona_server/install_percona_server.py
@@ -4,5 +4,5 @@ def install_percona_server(ps_version):
     ps_port=3317
     ps_container=f"pmm_ps_{ps_version}"
     print(f"Percona server version is: {ps_version}")
-    subprocess.run("docker network create pmm-qa | true")
-    subprocess.run(f"docker run -d --name={ps_container} -p {ps_port}:3307 phusion/baseimage:noble-1.0.1")
+    subprocess.run("docker network create pmm-qa || true", shell=True, check=True, text=True, capture_output=True)
+    subprocess.run(f"docker run -d --name={ps_container} -p {ps_port}:3307 phusion/baseimage:noble-1.0.1", shell=True, check=True, text=True, capture_output=True)
