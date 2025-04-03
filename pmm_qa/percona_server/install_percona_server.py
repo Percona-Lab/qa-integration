05ee7625c8aad03b3791f01dee2d2ac562b0cfaf
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-03 14:26:34 +0200

    PMM-7: install ps

--- pmm_qa/percona_server/install_percona_server.py
+++ pmm_qa/percona_server/install_percona_server.py
@@ -19,12 +19,12 @@ def install_percona_server(ps_version, query_source):
     run_command(f"docker exec {ps_container} init_pass=$(grep \"temporary password\" /var/log/mysqld.log | awk '{{print $NF}}' | tail -1)")
     run_command(f"docker exec {ps_container} echo init_pass")
 
-    # if(ps_version == 84):
-        # run_command(f"docker exec {ps_container} percona-release setup ps84lts")
-    # else:
-    #     raise Exception(f"Percona server version: {ps_version} is not supported")
+    if(ps_version == 84):
+        run_command(f"docker exec {ps_container} percona-release setup ps84lts")
+    else:
+        raise Exception(f"Percona server version: {ps_version} is not supported")
 
-    # run_command(f"docker exec {ps_container} DEBIAN_FRONTEND=noninteractive  apt -y install percona-server-server")
+    run_command(f"docker exec {ps_container} apt -y install percona-server-server")
     # mysql -u root -p -e "CREATE USER 'msandbox'@'%' IDENTIFIED BY 'msandbox'; GRANT ALL PRIVILEGES ON *.* TO 'msandbox'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
     # sysbench oltp_common - -db - driver = mysql - -mysql - db = test - -mysql - user = username - -mysql - password = password - -mysql - host = localhost - -mysql - port = 3306 - -tables = 10 - -table - size = 1000000
