81b3640b875438ab87e4580c60a8f88b14cd76a6
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-03 09:18:36 +0200

    PMM-7: Remove old container

--- pmm_qa/percona_server/install_percona_server.py
+++ pmm_qa/percona_server/install_percona_server.py
@@ -4,6 +4,7 @@ def install_percona_server(ps_version):
     ps_port=3317
     ps_container=f"pmm_ps_{ps_version}"
     print(f"Percona server version is: {ps_version}")
+    run_command(f"docker rm -f {ps_container} || true")
     run_command("docker network create pmm-qa || true")
     run_command(f"docker run -d --name={ps_container} -p {ps_port}:3307 phusion/baseimage:noble-1.0.1")
     run_command(f"docker cp ../client_container_ps_setup.sh {ps_container}:/")
