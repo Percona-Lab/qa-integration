f6072c2dcf05c427c811255320375cbed8f39d20
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-03 09:21:26 +0200

    PMM-7: install dependencies

--- pmm_qa/percona_server/install_percona_server.py
+++ pmm_qa/percona_server/install_percona_server.py
@@ -8,6 +8,10 @@ def install_percona_server(ps_version):
     run_command("docker network create pmm-qa || true")
     run_command(f"docker run -d --name={ps_container} -p {ps_port}:3307 phusion/baseimage:noble-1.0.1")
     run_command(f"docker cp ./client_container_ps_setup.sh {ps_container}:/")
+    run_command(f"docker exec {ps_container} apt-get update")
+    run_command(f"docker exec {ps_container} apt-get -y install wget curl git gnupg2 lsb-release")
+    run_command(f"docker exec {ps_container} apt-get -y install libaio1 libaio-dev libnuma-dev socat")
+    run_command(f"docker exec {ps_container} apt-get -y install sysbench")
 
 def run_command(cmd):
     subprocess.run(cmd, shell=True, check=True, text=True, capture_output=True)
