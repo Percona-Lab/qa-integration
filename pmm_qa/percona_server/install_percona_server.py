f09cdb8c5c1e69497d96f551461f336ea2a0dad8
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-03 14:24:01 +0200

    PMM-7: fix ps version

--- pmm_qa/percona_server/install_percona_server.py
+++ pmm_qa/percona_server/install_percona_server.py
@@ -16,6 +16,8 @@ def install_percona_server(ps_version, query_source):
     run_command(f"docker exec {ps_container} curl -O https://repo.percona.com/apt/percona-release_latest.generic_all.deb")
     run_command(f"docker exec {ps_container} apt -y install gnupg2 lsb-release ./percona-release_latest.generic_all.deb")
     run_command(f"docker exec {ps_container} apt update")
+    run_command(f"docker exec {ps_container} init_pass=$(grep \"temporary password\" /var/log/mysqld.log | awk '{{print $NF}}' | tail -1)")
+    run_command(f"docker exec {ps_container} echo init_pass")
 
     # if(ps_version == 84):
         # run_command(f"docker exec {ps_container} percona-release setup ps84lts")
@@ -23,10 +25,9 @@ def install_percona_server(ps_version, query_source):
     #     raise Exception(f"Percona server version: {ps_version} is not supported")
 
     # run_command(f"docker exec {ps_container} DEBIAN_FRONTEND=noninteractive  apt -y install percona-server-server")
-    # mysql - u root - p - e "CREATE USER 'msandbox'@'%' IDENTIFIED BY 'msandbox'; GRANT ALL PRIVILEGES ON *.* TO 'msandbox'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+    # mysql -u root -p -e "CREATE USER 'msandbox'@'%' IDENTIFIED BY 'msandbox'; GRANT ALL PRIVILEGES ON *.* TO 'msandbox'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-    # sysbench
-    # oltp_common - -db - driver = mysql - -mysql - db = test - -mysql - user = username - -mysql - password = password - -mysql - host = localhost - -mysql - port = 3306 - -tables = 10 - -table - size = 1000000
+    # sysbench oltp_common - -db - driver = mysql - -mysql - db = test - -mysql - user = username - -mysql - password = password - -mysql - host = localhost - -mysql - port = 3306 - -tables = 10 - -table - size = 1000000
     # prepare
     # service
     # mysql
