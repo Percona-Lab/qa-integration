7e00cbe36258f3b6a6190a3f22175532d20abd49
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-09 07:45:24 +0200

    PMM-7: install ps

--- pmm_qa/pmm-framework.py
+++ pmm_qa/pmm-framework.py
@@ -187,9 +187,11 @@ def setup_ps(db_type, db_version=None, db_config=None, args=None):
     ps_version = os.getenv('PS_VERSION') or db_version or database_configs[db_type]["versions"][-1]
     ps_version_int = int(ps_version.replace(".", ""))
     print(f'Setup type for PS is {setup_type_value}')
+    print(f'Args are: {args}')
     if ps_version_int >= 84:
         # Define environment variables for playbook
         env_vars = {
+            'PMM_SERVER_IP': args.pmm_server_ip or container_name or '127.0.0.1',
             'SETUP_TYPE': setup_type_value,
             'SERVICES_COUNT': 3
         }
