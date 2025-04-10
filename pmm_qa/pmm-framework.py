39960dea3ae7efe9fec2b37530c69af3a392fa44
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-10 14:38:37 +0200

    PMM-7: install ps

--- pmm_qa/pmm-framework.py
+++ pmm_qa/pmm-framework.py
@@ -101,6 +101,7 @@ def run_ansible_playbook(playbook_filename, env_vars, args):
         cmdline='-l localhost, --connection=local',
         envvars=env_vars,
         suppress_env_files=True,
+        verbose= 5,
     )
 
     print(f'{playbook_filename} playbook execution {r.status}')
