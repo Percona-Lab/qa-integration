6779f455450da257811fc1b6366e110212807d6f
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-25 09:26:29 +0200

    PMM-12153: Split group replication

--- pmm_qa/pmm-framework.py
+++ pmm_qa/pmm-framework.py
@@ -103,7 +103,7 @@ def run_ansible_playbook(playbook_filename, env_vars, args):
         cmdline='-l localhost, --connection=local',
         envvars=env_vars,
         suppress_env_files=True,
-        verbosity=5,
+        verbosity=int(args.verbosity_level),
     )
 
     print(f'{playbook_filename} playbook execution {r.status}')
