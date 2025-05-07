a484f8259c3c5b131d9a250fb68ba6c53fdacc79
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-07 15:29:26 +0200

    PMM-7: Add pgsql

--- pmm_qa/postgresql/data/workload.sql
+++ pmm_qa/postgresql/data/workload.sql
@@ -1,28 +1,27 @@
--- begin a transaction
+\set aid random(1, 10000)
+\set delta random(-100, 100)
+\set bid random(1, 10)
+\set tid random(1, 50)
+
 BEGIN;
 
--- simulate user account lookup
 SELECT abalance
-FROM pgbench_accounts
-WHERE aid = :aid;
+  FROM pgbench_accounts
+ WHERE aid = :aid;
 
--- simulate joining branch and teller info
 SELECT b.bid, b.bname, t.tid, t.tname
-FROM pgbench_branches b
-JOIN pgbench_tellers t ON b.bid = t.bid
-WHERE b.bid = :bid AND t.tid = :tid;
+  FROM pgbench_branches b
+  JOIN pgbench_tellers t ON b.bid = t.bid
+ WHERE b.bid = :bid AND t.tid = :tid;
 
--- conditional balance transfer
 UPDATE pgbench_accounts
-SET abalance = abalance - :delta
-WHERE aid = :aid AND abalance >= :delta;
+   SET abalance = abalance - :delta
+ WHERE aid = :aid AND abalance >= :delta;
 
--- deposit to another account
 UPDATE pgbench_accounts
-SET abalance = abalance + :delta
-WHERE aid = :aid + 1;
+   SET abalance = abalance + :delta
+ WHERE aid = :aid + 1;
 
--- insert transaction log
 INSERT INTO pgbench_history (tid, bid, aid, delta, mtime)
 VALUES (:tid, :bid, :aid, :delta, CURRENT_TIMESTAMP);
 
