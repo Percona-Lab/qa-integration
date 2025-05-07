82e01f65a7389872e5910afa1fd8a70a94e47570
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-07 15:34:56 +0200

    PMM-7: Add pgsql

--- pmm_qa/postgresql/data/sample_data.sql
+++ pmm_qa/postgresql/data/sample_data.sql
@@ -12,3 +12,27 @@ FROM generate_series(1, 50) AS i;
 INSERT INTO pgbench_accounts (aid, bid, abalance)
 SELECT i, (i % 10) + 1, 1000
 FROM generate_series(1, 10000) AS i;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_name = 'pgbench_branches'
+      AND column_name = 'bname'
+  ) THEN
+    ALTER TABLE pgbench_branches ADD COLUMN bname TEXT DEFAULT 'Branch';
+  END IF;
+END$$;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_name = 'pgbench_tellers'
+      AND column_name = 'tname'
+  ) THEN
+    ALTER TABLE pgbench_tellers ADD COLUMN tname TEXT DEFAULT 'Teller';
+  END IF;
+END$$;
--- pmm_qa/postgresql/data/workload.sql
+++ pmm_qa/postgresql/data/workload.sql
@@ -3,30 +3,6 @@
 \set bid random(1, 10)
 \set tid random(1, 50)
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM information_schema.columns
-    WHERE table_name = 'pgbench_branches'
-      AND column_name = 'bname'
-  ) THEN
-    ALTER TABLE pgbench_branches ADD COLUMN bname TEXT DEFAULT 'Branch';
-  END IF;
-END$$;
-
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM information_schema.columns
-    WHERE table_name = 'pgbench_tellers'
-      AND column_name = 'tname'
-  ) THEN
-    ALTER TABLE pgbench_tellers ADD COLUMN tname TEXT DEFAULT 'Teller';
-  END IF;
-END$$;
-
 BEGIN;
 
 SELECT abalance
