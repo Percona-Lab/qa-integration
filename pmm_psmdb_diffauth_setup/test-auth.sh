6b2d7fc93bdc20c855cc71d052c9fb4bb4aa80aa
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-15 12:44:53 +0200

    PMM-7: Fix PSMDB setup

--- pmm_psmdb_diffauth_setup/test-auth.sh
+++ pmm_psmdb_diffauth_setup/test-auth.sh
@@ -20,14 +20,14 @@ if [[ -n "$PSMDB_VERSION" ]] && [[ "$PSMDB_VERSION" == *"4.2."* ]]; then
 fi
 
 #Generate certificates for tests
-rm -rf easy-rsa pki certs && mkdir certs
-git clone https://github.com/OpenVPN/easy-rsa.git
-./easy-rsa/easyrsa3/easyrsa init-pki
-./easy-rsa/easyrsa3/easyrsa --req-cn=Percona --batch build-ca nopass
-./easy-rsa/easyrsa3/easyrsa --req-ou=server --subject-alt-name=DNS:pmm-server --batch build-server-full pmm-server nopass
-./easy-rsa/easyrsa3/easyrsa --req-ou=server --subject-alt-name=DNS:psmdb-server --batch build-server-full psmdb-server nopass
-./easy-rsa/easyrsa3/easyrsa --req-ou=client --batch build-client-full pmm-test nopass
-openssl dhparam -out certs/dhparam.pem 2048
+#rm -rf easy-rsa pki certs && mkdir certs
+#git clone https://github.com/OpenVPN/easy-rsa.git
+#./easy-rsa/easyrsa3/easyrsa init-pki
+#./easy-rsa/easyrsa3/easyrsa --req-cn=Percona --batch build-ca nopass
+#./easy-rsa/easyrsa3/easyrsa --req-ou=server --subject-alt-name=DNS:pmm-server --batch build-server-full pmm-server nopass
+#./easy-rsa/easyrsa3/easyrsa --req-ou=server --subject-alt-name=DNS:psmdb-server --batch build-server-full psmdb-server nopass
+#./easy-rsa/easyrsa3/easyrsa --req-ou=client --batch build-client-full pmm-test nopass
+#openssl dhparam -out certs/dhparam.pem 2048
 
 #cp pki/ca.crt certs/ca-certs.pem
 #cp pki/private/pmm-server.key certs/certificate.key
