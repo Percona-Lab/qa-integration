2d52a0417a4d34cebbaaddfa9027483bec7e124e
yurkovychv <vasyl.yurkovych@percona.com>
2025-08-18 21:06:21 +0300

    PMM-14185 handle gssapi

--- pmm_psmdb-pbm_setup/configure-agents.sh
+++ pmm_psmdb-pbm_setup/configure-agents.sh
@@ -54,7 +54,6 @@ do
       docker compose -f docker-compose-rs.yaml exec -T $node pmm-admin add mongodb --enable-all-collectors --agent-password=mypass --cluster=replicaset --replication-set=rs --host=${node} --port=27017 ${node}${gssapi_service_name_part}_${random_number}
     else
       echo
-      echo "docker compose -f docker-compose-rs.yaml exec -T $node pmm-admin add mongodb --enable-all-collectors --agent-password=mypass --cluster=replicaset --replication-set=rs ${client_credentials_flags[*]} --host=${node} --port=27017 ${node}${gssapi_service_name_part}_${random_number}"
       docker compose -f docker-compose-rs.yaml exec -T $node pmm-admin add mongodb --enable-all-collectors --agent-password=mypass --cluster=replicaset --replication-set=rs ${client_credentials_flags[*]} --host=${node} --port=27017 ${node}${gssapi_service_name_part}_${random_number}
     fi
 done
